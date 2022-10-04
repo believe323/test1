da146852855f6a7c4837647b1652f18dc6a0896a
23098 <2309821334@qq.com>
2022-10-04 20:01:07 +0800

    新的仓库

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,24 +1,70 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-//模板
-template<typename T>
-void change(T& t1,T& t2){
-    T tem = t1;
-    t1 = t2;
-    t2 = tem;
+
+//实现排序功能
+template<typename T,typename A>
+void sort(T* arr,A size){
+    int i,j;
+    i=j=0;
+    for(i=0;i<size-1;i++){
+        for(j=0;j<size-i;j++){
+            if(arr[j]<arr[j+1]){
+                T tem = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tem;
+            }
+        }
+    }
+}
+
+void test01(){
+    char ch[] = "abcdefg";
+    sort(ch,strlen(ch));
+    cout<<ch<<endl;
+}
+
+//类模板
+template<class T1,class T2>
+class Person{
+    public:
+        Person(T1 age,T2 name){
+            m_age = age;
+            m_name = name;
+        }
+
+        void getmem(){
+            cout<<m_age<<"  "<<m_name<<endl;
+        }
+
+        T1 m_age;
+        T2 m_name;
+    };
+
+//关于类模板成员函数创建的时机
+
+//类模板作为函数参数的三种方法
+//1.显示具体参数类型
+void test02(Person<int,string>& p){
+    p.getmem();
+}
+//2.参数模板化
+template<class T1,class T2>
+void test03(Person<T1,T2>&p){
+    p.getmem();
+}
+
+template<class T>
+void test04(T& p){
+    cout<<typeid(T).name()<<endl;
+    p.getmem();
 }
 
 int main(){
-    int a = 20;
-    int b = 3;
-    change(a,b);
-    cout<<a<<" "<<b<<endl;
-    char c = 'a';
-    char d = 'b';
-    change(c,d);
-    //1.自动类型推导
-    //2.显示指定类型
-
+    Person<int,string>p(12,"小张");
+    test02(p);
+    test03(p);
+    test04(p);
     return 0;
 }
